3ff5581c6e02eb531784f0d725b9244d62b64e38
lifeifei1991 <18516291046@163.com>
2017-05-21 14:28:17 +0800

    add mean

--- try.py
+++ try.py
@@ -1,9 +1,49 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+import MySQLdb
+import numpy as np 
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import mapper, sessionmaker
 from sqlalchemy import Table, MetaData, Column, Integer, String, LargeBinary, Float, DateTime
+def my_avg(horizontal, vertical, ordinate, macAddr):   #求出某一坐标下某一mac地址下场强的均值
+    conn= MySQLdb.connect(
+        host='localhost',
+        port = 3306,
+        user='root',
+        passwd='feifei_1',
+        db ='wifi',
+        )
+    cur = conn.cursor()
+    #cur.execute("select * from wifiset")
+    MySqlSelectDataCM = "select RSSI from wifiset where horizontal = %s and vertical = %s and ordinate = %s and macAddr = %s"
+    #aa = cur.execute("select RSSI from wifiset where horizontal = %s and vertical = %s and ordinate = %s and macAddr = %s",[x,y,z,h])
+    aa = cur.execute(MySqlSelectDataCM,[horizontal,vertical,ordinate,macAddr])
+    print aa
+    info = cur.fetchmany(aa)
+    list = []
+    for ii in info:
+        list.append(ii)
+    print list
+    newlist = []
+    for i in list:
+        newlist.append(i[0])
+    print newlist
+    intlist = map(int, newlist)
+    temp = []
+    for i in intlist:
+        temp.append(i)
+    print temp
+    avg = np.mean(temp)
+    print ('chang qiang jun zhi:', avg)
+    cur.close()
+    conn.commit()
+    conn.close()
+    return avg
+
+m = my_avg(1, 2, 3, '88:25:93:08:73:0c')
+print m
+
 engine = create_engine("mysql+pymysql://root:feifei_1@localhost/wifi",encoding='utf-8', echo=True)
 metadata = MetaData()
 user = Table('wifiset', metadata,
@@ -36,4 +76,5 @@ Session_class = sessionmaker(bind=engine)  # 实例和engine绑定
 Session = Session_class()  # 生成session实例，相当于游标
 #my_user = Session.query(User).filter_by(ID=1).first()  # 查询
 #print(my_user.horizontal, my_user.vertical, my_user.ordinate)
-print(Session.query(User.horizontal,User.vertical,User.ordinate).all() )
+#print(Session.query(User.macAddr).all() )  #列出mac地址
+
